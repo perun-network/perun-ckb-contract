7deda3ab3339715d6f88d555419125c42c30b506
Norbert Dzikowski <norbert@perun.network>
2023-03-24 15:47:36 +0100

    perun-common: Use moleculec 0.7.3 for binding generation

--- contracts/perun-common/src/lib.rs
+++ contracts/perun-common/src/lib.rs
@@ -1,13 +1,3 @@
 #![no_std]
 
-use ckb_std::ckb_types::bytes::Bytes;
-
-// Create a struct
-#[derive(Debug)]
-pub struct ChannelParameters {
-    // Participants is an array of addresses.
-    pub participants: [Address; 2],
-    pub nonce: Bytes,
-}
-
-pub type Address = [u8; 32];
+pub mod perun_types;
