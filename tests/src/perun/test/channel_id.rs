e15b3e7bb8099a5c3c252059e9b4e1fb35854fe9
Norbert Dzikowski <norbert@perun.network>
2023-04-12 09:36:32 +0200

    tests: Give channel_id a random constructor

--- tests/src/perun/test/channel_id.rs
+++ tests/src/perun/test/channel_id.rs
@@ -1,1 +1,14 @@
-pub type ChannelId = u32;
+use rand::Rng;
+
+#[derive(Clone, Copy)]
+pub struct ChannelId(u32);
+
+impl ChannelId {
+    pub fn new() -> Self {
+        ChannelId(0)
+    }
+
+    pub fn new_random() -> Self {
+        ChannelId(rand::thread_rng().gen())
+    }
+}
