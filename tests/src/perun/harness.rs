1824998b49630dbd8bc887838932e4c935325932
Norbert Dzikowski <norbert@perun.network>
2023-04-12 09:37:06 +0200

    tests/harness: Allow Env to build multiple perun scripts

--- tests/src/perun/harness.rs
+++ tests/src/perun/harness.rs
@@ -24,60 +24,83 @@ pub struct Env {
     pub always_success_script_dep: CellDep,
 }
 
-// prepare_env prepares the given context to be used for running Perun
-// tests.
-pub fn prepare_env(context: &mut Context) -> Result<Env, perun::error::Error> {
-    // Perun contracts.
-    let pcls: Bytes = Loader::default().load_binary("perun-channel-lockscript");
-    let pcts: Bytes = Loader::default().load_binary("perun-channel-typescript");
-    let pfls: Bytes = Loader::default().load_binary("perun-funds-lockscript");
-    // Deploying the contracts returns the cell they are deployed in.
-    let pcls_out_point = context.deploy_cell(pcls);
-    let pcts_out_point = context.deploy_cell(pcts);
-    let pfls_out_point = context.deploy_cell(pfls);
-    // Auxiliary contracts.
-    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
+impl Env {
+    // prepare_env prepares the given context to be used for running Perun
+    // tests.
+    pub fn new(context: &mut Context) -> Result<Env, perun::error::Error> {
+        // Perun contracts.
+        let pcls: Bytes = Loader::default().load_binary("perun-channel-lockscript");
+        let pcts: Bytes = Loader::default().load_binary("perun-channel-typescript");
+        let pfls: Bytes = Loader::default().load_binary("perun-funds-lockscript");
+        // Deploying the contracts returns the cell they are deployed in.
+        let pcls_out_point = context.deploy_cell(pcls);
+        let pcts_out_point = context.deploy_cell(pcts);
+        let pfls_out_point = context.deploy_cell(pfls);
+        // Auxiliary contracts.
+        let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
 
-    // Prepare scripts.
-    // Perun scripts.
-    let pcls_script = context
-        .build_script(&pcls_out_point, Default::default())
-        .ok_or("perun-channel-lockscript")?;
-    let pcts_script = context
-        .build_script(&pcts_out_point, Default::default())
-        .ok_or("perun-channel-typescript")?;
-    let pfls_script = context
-        .build_script(&pfls_out_point, Default::default())
-        .ok_or("perun-funds-lockscript")?;
-    let pcls_script_dep = CellDep::new_builder()
-        .out_point(pcls_out_point.clone())
-        .build();
-    let pcts_script_dep = CellDep::new_builder()
-        .out_point(pcts_out_point.clone())
-        .build();
-    let pfls_script_dep = CellDep::new_builder()
-        .out_point(pfls_out_point.clone())
-        .build();
-    // Auxiliary scripts.
-    let always_success_script = context
-        .build_script(&always_success_out_point, Default::default())
-        .expect("always_success");
-    let always_success_script_dep = CellDep::new_builder()
-        .out_point(always_success_out_point.clone())
-        .build();
+        // Prepare scripts.
+        // Perun scripts.
+        let pcls_script = context
+            .build_script(&pcls_out_point, Default::default())
+            .ok_or("perun-channel-lockscript")?;
+        let pcts_script = context
+            .build_script(&pcts_out_point, Default::default())
+            .ok_or("perun-channel-typescript")?;
+        let pfls_script = context
+            .build_script(&pfls_out_point, Default::default())
+            .ok_or("perun-funds-lockscript")?;
+        let pcls_script_dep = CellDep::new_builder()
+            .out_point(pcls_out_point.clone())
+            .build();
+        let pcts_script_dep = CellDep::new_builder()
+            .out_point(pcts_out_point.clone())
+            .build();
+        let pfls_script_dep = CellDep::new_builder()
+            .out_point(pfls_out_point.clone())
+            .build();
+        // Auxiliary scripts.
+        let always_success_script = context
+            .build_script(&always_success_out_point, Default::default())
+            .expect("always_success");
+        let always_success_script_dep = CellDep::new_builder()
+            .out_point(always_success_out_point.clone())
+            .build();
 
-    Ok(Env {
-        pcls_out_point,
-        pcts_out_point,
-        pfls_out_point,
-        always_success_out_point,
-        pcls_script,
-        pcts_script,
-        pfls_script,
-        pcls_script_dep,
-        pcts_script_dep,
-        pfls_script_dep,
-        always_success_script,
-        always_success_script_dep,
-    })
+        Ok(Env {
+            pcls_out_point,
+            pcts_out_point,
+            pfls_out_point,
+            always_success_out_point,
+            pcls_script,
+            pcts_script,
+            pfls_script,
+            pcls_script_dep,
+            pcts_script_dep,
+            pfls_script_dep,
+            always_success_script,
+            always_success_script_dep,
+        })
+    }
+
+    pub fn build_pcls(&self, context: &mut Context, args: Bytes) -> Script {
+        let pcls_out_point = &self.pcls_out_point;
+        context
+            .build_script(pcls_out_point, args)
+            .expect("perun-channel-lockscript")
+    }
+
+    pub fn build_pcts(&self, context: &mut Context, args: Bytes) -> Script {
+        let pcts_out_point = &self.pcts_out_point;
+        context
+            .build_script(pcts_out_point, args)
+            .expect("perun-channel-typescript")
+    }
+
+    pub fn build_pfls(&self, context: &mut Context, args: Bytes) -> Script {
+        let pfls_out_point = &self.pfls_out_point;
+        context
+            .build_script(pfls_out_point, args)
+            .expect("perun-funds-lockscript")
+    }
 }
