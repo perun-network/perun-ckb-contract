d4380f5c402256b740dd81fe21867f76cae1b578
Jan Bormet <jan.bormet@bormet.info>
2023-04-13 11:21:42 +0200

    common: Remove comment that signature verification is just a draft

--- contracts/perun-common/src/sig.rs
+++ contracts/perun-common/src/sig.rs
@@ -2,7 +2,6 @@ use k256::ecdsa::{VerifyingKey, Signature, signature::{hazmat::PrehashVerifier}}
 
 use crate::{error::Error, helpers::blake2b256};
 
-// TODO: This is just a draft!
 pub fn verify_signature(msg: &[u8], sig: &[u8], key: &[u8]) -> Result<(), Error> {
     let msg_hash = blake2b256(msg);
     let signature = Signature::from_der(sig)?;
