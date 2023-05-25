d1d0781bb8e45ee67417c6a4134530df709d4024
Jan Bormet <jan.bormet@bormet.info>
2023-05-25 15:03:14 +0200

    tests: Add utility for adding some capacity to party a's balance

--- tests/src/perun/test/transaction/common.rs
+++ tests/src/perun/test/transaction/common.rs
@@ -1,9 +1,11 @@
 use ckb_occupied_capacity::Capacity;
 use ckb_testtool::{
     bytes,
-    ckb_types::packed::{Byte32, CellOutput, OutPoint},
+    ckb_types::{packed::{Byte32, CellOutput, OutPoint}, prelude::{Unpack, Pack}},
     context::Context,
 };
+use molecule::prelude::{Entity, Builder};
+use perun_common::perun_types::Balances;
 
 use crate::perun;
 
@@ -29,4 +31,11 @@ pub fn create_cells(ctx: &mut Context, hash: Byte32, outputs: Vec<(CellOutput, b
         let out_point = OutPoint::new(hash.clone(), i as u32);
         ctx.create_cell_with_out_point(out_point, output, data);
     }
+}
+
+pub fn add_cap_to_a(balances: &Balances, cap: Capacity) -> Balances {
+    let bal_a: u64 = balances.ckbytes().nth0().unpack();
+    balances.clone().as_builder().ckbytes(
+        balances.ckbytes().as_builder().nth0(
+            (cap.as_u64() + bal_a).pack()).build()).build()
 }
