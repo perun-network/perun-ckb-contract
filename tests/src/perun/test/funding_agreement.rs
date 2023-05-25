8661d88993cb7bfb1ca97bde7b6fd106656398ad
Jan Bormet <jan.bormet@bormet.info>
2023-05-25 14:57:39 +0200

    tests: Add functionality to FundingAgreement and AssetRegister

--- tests/src/perun/test/funding_agreement.rs
+++ tests/src/perun/test/funding_agreement.rs
@@ -1,28 +1,36 @@
-use std::collections::HashMap;
-
 use ckb_occupied_capacity::Capacity;
-use ckb_testtool::ckb_types::packed::{Byte as PackedByte, Byte32, Uint64, Script};
+use ckb_testtool::ckb_types::packed::{Byte as PackedByte, Script};
 use ckb_testtool::ckb_types::prelude::*;
 use ckb_testtool::context::Context;
 use ckb_types::bytes::Bytes;
 use k256::elliptic_curve::sec1::ToEncodedPoint;
 use k256::PublicKey;
 use perun_common::perun_types::{
-    self, Balances, BalancesBuilder, ParticipantBuilder, SEC1EncodedPubKeyBuilder, CKByteDistribution, SUDTAsset, SUDTAllocation, SUDTBalances, SUDTDistribution,
+    self, Balances, CKByteDistribution, ParticipantBuilder,
+    SEC1EncodedPubKeyBuilder, SUDTAllocation, SUDTAsset, SUDTBalances, SUDTDistribution,
 };
 
 use crate::perun;
 
 #[derive(Debug, Clone)]
-pub struct FundingAgreement{
-    entries: Vec<FundingAgreementEntry>, 
+pub struct FundingAgreement {
+    entries: Vec<FundingAgreementEntry>,
     register: AssetRegister,
 }
 
 impl FundingAgreement {
+    pub fn register(&self) -> &AssetRegister {
+        &self.register
+    }
+
+    pub fn has_udts(&self) -> bool {
+        self.register.len() > 0
+    }
+
     pub fn new_with_capacities<P: perun::Account>(caps: Vec<(P, u64)>) -> Self {
-        FundingAgreement{
-            entries: caps.iter()
+        FundingAgreement {
+            entries: caps
+                .iter()
                 .enumerate()
                 .map(|(i, (acc, c))| FundingAgreementEntry {
                     ckbytes: *c,
@@ -32,7 +40,35 @@ impl FundingAgreement {
                 })
                 .collect(),
             register: AssetRegister::new(),
-            }
+        }
+    }
+
+    pub fn new_with_capacities_and_sudt<P: perun::Account>(
+        caps: Vec<(P, u64)>,
+        asset: &Script,
+        max_cap: u64,
+        asset_amt: Vec<(P, u128)>,
+    ) -> Self {
+        let mut r = AssetRegister::new();
+        let a = r.register_asset(
+            SUDTAsset::new_builder()
+                .type_script(asset.clone())
+                .max_capacity(max_cap.pack())
+                .build(),
+        );
+        FundingAgreement {
+            entries: caps
+                .iter()
+                .enumerate()
+                .map(|(i, (acc, c))| FundingAgreementEntry {
+                    ckbytes: *c,
+                    sudts: vec![(a, asset_amt.get(i).unwrap().1)],
+                    index: i as u8,
+                    pub_key: acc.public_key(),
+                })
+                .collect(),
+            register: r,
+        }
     }
 
     pub fn content(&self) -> &Vec<FundingAgreementEntry> {
@@ -91,7 +127,7 @@ impl FundingAgreement {
     /// already funded their part.
     pub fn mk_balances(&self, indices: Vec<u8>) -> Result<Balances, perun::Error> {
         let mut ckbytes = [0u64; 2];
-        let sudts = self.register.get_SUDTAssets();
+        let sudts = self.register.get_sudtassets();
         let mut sudt_dist: Vec<[u128; 2]> = Vec::new();
         for _ in 0..sudts.len() {
             sudt_dist.push([0u128, 0]);
@@ -108,22 +144,28 @@ impl FundingAgreement {
         }
         let mut sudt_alloc: Vec<SUDTBalances> = Vec::new();
         for (i, asset) in sudts.iter().enumerate() {
-            sudt_alloc.push(SUDTBalances::new_builder()
-            .asset(asset.clone())
-            .distribution(SUDTDistribution::new_builder()
-                .nth0(sudt_dist[i][0].pack())
-                .nth1(sudt_dist[i][1].pack())
-                .build())
-            .build());
+            sudt_alloc.push(
+                SUDTBalances::new_builder()
+                    .asset(asset.clone())
+                    .distribution(
+                        SUDTDistribution::new_builder()
+                            .nth0(sudt_dist[i][0].pack())
+                            .nth1(sudt_dist[i][1].pack())
+                            .build(),
+                    )
+                    .build(),
+            );
         }
-        
+
         println!("mkbalances ckbytes: {:?}", ckbytes);
 
         Ok(Balances::new_builder()
-            .ckbytes(CKByteDistribution::new_builder()
-                        .nth0(ckbytes[0].pack())
-                        .nth1(ckbytes[1].pack())
-                        .build())
+            .ckbytes(
+                CKByteDistribution::new_builder()
+                    .nth0(ckbytes[0].pack())
+                    .nth1(ckbytes[1].pack())
+                    .build(),
+            )
             .sudts(SUDTAllocation::new_builder().set(sudt_alloc).build())
             .build())
     }
@@ -136,19 +178,33 @@ impl FundingAgreement {
             .ok_or("unknown index")?;
         Ok(entry.ckbytes)
     }
-    pub fn expected_sudts_funding_for(&self, index: u8) -> Result<Vec<(Script, Capacity, u128)>, perun::Error> {
-        let entry = self.entries
+
+    pub fn sudt_max_cap_sum(&self) -> u64 {
+        self.register.get_sudtassets().iter().fold(0u64, |old, asset| {
+            old + Capacity::shannons(asset.max_capacity().unpack()).as_u64()
+        })
+    }
+
+    pub fn expected_sudts_funding_for(
+        &self,
+        index: u8,
+    ) -> Result<Vec<(Script, Capacity, u128)>, perun::Error> {
+        let entry = self
+            .entries
             .iter()
             .find(|entry| entry.index == index)
             .ok_or("unknown index")?;
-        entry.sudts.iter().map(|(asset, amount)| {
-            let sudt_asset = self.register.get_SUDTAsset(asset).ok_or("unknown asset")?;
-            let sudt_script = sudt_asset.type_script();
-            let sudt_capacity = Capacity::shannons(sudt_asset.max_capacity().unpack());
-            Ok((sudt_script, sudt_capacity, *amount))
-        }).collect::<Result<Vec<(Script, Capacity, u128)>, perun::Error>>()
+        entry
+            .sudts
+            .iter()
+            .map(|(asset, amount)| {
+                let sudt_asset = self.register.get_sudtasset(asset).ok_or("unknown asset")?;
+                let sudt_script = sudt_asset.type_script();
+                let sudt_capacity = Capacity::shannons(sudt_asset.max_capacity().unpack());
+                Ok((sudt_script, sudt_capacity, *amount))
+            })
+            .collect::<Result<Vec<(Script, Capacity, u128)>, perun::Error>>()
     }
-
 }
 
 #[derive(Debug, Clone)]
@@ -175,30 +231,52 @@ impl Default for Asset {
 }
 
 #[derive(Debug, Clone)]
-struct AssetRegister {
-    counter: u32,
-    map: HashMap<Asset, SUDTAsset>,
+pub struct AssetRegister {
+    assets: Vec<(Asset, SUDTAsset)>,
 }
 
 impl AssetRegister {
     fn new() -> Self {
         AssetRegister {
-            counter: 0,
-            map: HashMap::new(),
+            assets: Vec::new(),
         }
     }
 
-    pub fn register_asset(&mut self, sudt_asset: SUDTAsset) -> Asset {
-        let asset = Asset(self.counter);
-        self.map.insert(asset, sudt_asset);
-        self.counter += 1;
-        return asset;
-    }
-    pub fn get_SUDTAsset(&self, asset: &Asset) -> Option<&SUDTAsset> {
-        self.map.get(asset)
+    pub fn len(&self) -> usize {
+        self.assets.len()
     }
 
-    pub fn get_SUDTAssets(&self) -> Vec<SUDTAsset> {
-        self.map.values().cloned().collect()
+    pub fn register_asset(&mut self, sudt_asset: SUDTAsset) -> Asset {
+        let asset = Asset(self.assets.len() as u32);
+        self.assets.push((asset, sudt_asset));
+        return asset;
     }
-} 
+    pub fn get_sudtasset(&self, asset: &Asset) -> Option<&SUDTAsset> {
+        match self.assets.get(asset.0 as usize) {
+            Some((_, sudt_asset)) => Some(sudt_asset),
+            None => None,
+        }
+    }
+
+    pub fn get_asset(&self, sudt_asset: SUDTAsset) -> Option<&Asset> {
+        match self.assets.iter().find(|(_, a)| a.as_slice()[..] == sudt_asset.as_slice()[..]) {
+            Some((asset, _)) => Some(asset),
+            None => None,
+        }
+    }
+
+    pub fn guess_asset_from_script(&self, script: &Script) -> Option<&Asset> {
+        match self
+            .assets
+            .iter()
+            .find(|(_, sudt_asset)| sudt_asset.type_script().as_slice()[..] == script.as_slice()[..])
+        {
+            Some((asset, _)) => Some(asset),
+            None => None,
+        }
+    }
+
+    pub fn get_sudtassets(&self) -> Vec<SUDTAsset> {
+        self.assets.iter().map(|(_, a)| a.clone()).collect()
+    }
+}
