33c584584048827330b02915f3e701f6d035aeeb
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2025-04-28 11:53:17 +0200

    chore: remove comments

--- contracts/perun-vchannel-typescript/src/lib.rs
+++ contracts/perun-vchannel-typescript/src/lib.rs
@@ -1,13 +1,3 @@
-// #![cfg_attr(not(feature = "library"), no_std)]
-// #![allow(special_module_name)]
-// #![allow(unused_attributes)]
-// #[cfg(feature = "library")]
-// mod main;
-// #[cfg(feature = "library")]
-// pub use main::program_entry;
-
-// extern crate alloc;
-
 #![cfg_attr(not(feature = "library"), no_std)]
 #![allow(special_module_name)]
 #![allow(unused_attributes)]
